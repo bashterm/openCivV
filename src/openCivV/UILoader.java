c40a1807ad5cf64e70fae2ff9cc18f785762e754
890156128 <890156128@S067W100-S04.rcsd.dom>
2016-12-09 10:13:06 -0500

    Added window thing

--- src/openCivV/UILoader.java
+++ src/openCivV/UILoader.java
@@ -1,5 +1,21 @@
 package openCivV;
 
-public class UILoader {
-
+import javax.swing.*;
+import java.awt.EventQueue;
+public class UILoader extends JFrame {
+	public static void main(String[] args){
+		EventQueue.invokeLater(() ->{
+			UILoader load= new UILoader();
+			load.setVisible(true);;
+		});
+	}
+	public UILoader() {
+		initUI();
+	}
+	private void initUI(){
+		setTitle("Open Civ V");
+		setSize(640,360);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
