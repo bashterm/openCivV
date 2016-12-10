5217fb42aabf8b937cdebd6fc47fd2354401cc44
Aidan Sciortino <2001flyingpigs14@gmail.com>
2016-12-10 11:48:00 -0500

    Update UILoader
    Tried to make the menu button right aligned using flowLayout built into
    swing. However, now the code crashes. Not Sure how to fix this. TODO:
    Read section of Core Java on Swing

--- src/openCivV/UILoader.java
+++ src/openCivV/UILoader.java
@@ -5,45 +5,40 @@ import java.awt.color.*;
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
+
 public class UILoader {
 	static JFrame frame = new JFrame();
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		initUI();
 	}
+
 	public UILoader() {
 		initUI();
 	}
-	private static void initUI(){
-		
+
+	private static void initUI() {
+
 		frame.setTitle("Open Civ V");
-		frame.setSize(1280,720);
+		frame.setSize(1280, 720);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		
+
 		frame.setVisible(true);
 		JButton menuButton = new customButton("Menu");
-		menuButton.addActionListener((ActionEvent event) ->{
+		menuButton.addActionListener((ActionEvent event) -> {
 			mainMenu.mainMenu();
 		});
-		
-		createLayout(menuButton);
+
+		frame.add(createLayout(menuButton));
 	}
-	 private static void createLayout(JComponent... arg) {
 
-	        Container pane = frame.getContentPane();
-	        GroupLayout gl = new GroupLayout(pane);
-	        pane.setLayout(gl);
+	private static JPanel createLayout(JComponent... arg) {
+		FlowLayout menuLayout = new FlowLayout(FlowLayout.TRAILING);
+		final JPanel gl = new JPanel();
+		gl.setLayout(menuLayout);
+		gl.add(arg[0]);
+		return gl;
+	}
 
-	        gl.setAutoCreateContainerGaps(true);
-
-	        gl.setHorizontalGroup(gl.createSequentialGroup()
-	                .addComponent(arg[0])
-	        );
-
-	        gl.setVerticalGroup(gl.createSequentialGroup()
-	                .addComponent(arg[0])
-	        );
-	 }
-	 
-	 
 }
