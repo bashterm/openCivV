778605f31dc22a494449a9c2a984e2dc36118d42
Aidan Sciortino <2001flyingpigs14@gmail.com>
2016-12-10 11:48:45 -0500

    Beautify Code
    Just formatted code using built in eclipse formatter. Looks better now.
    No functionality added, it's just easier to read.

--- src/openCivV/customButton.java
+++ src/openCivV/customButton.java
@@ -7,10 +7,10 @@ import java.awt.Font;
 import java.awt.color.*;
 
 public class customButton extends JButton {
-	public customButton(String text){
+	public customButton(String text) {
 		setText(text);
 		setBackground(Color.BLACK);
 		setForeground(Color.WHITE);
-		setFont(new Font("Tahoma", Font.BOLD,12));
+		setFont(new Font("Tahoma", Font.BOLD, 12));
 	}
 }
--- src/openCivV/mainMenu.java
+++ src/openCivV/mainMenu.java
@@ -9,43 +9,43 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-public class mainMenu extends JFrame{
-	private void initUI(){
+public class mainMenu extends JFrame {
+	private void initUI() {
 		setTitle("Menu");
-		setSize(200,400);
+		setSize(200, 400);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
+
 		JButton quitButton = new customButton("Quit");
-		quitButton.addActionListener((ActionEvent event) ->{
+		quitButton.addActionListener((ActionEvent event) -> {
 			System.exit(0);
 		});
 		createLayout(quitButton);
 	}
-	public mainMenu(){
+
+	public mainMenu() {
 		initUI();
 	}
-	public static void mainMenu(){
-		EventQueue.invokeLater(() ->{
-			mainMenu men= new mainMenu();
-			men.setVisible(true);;
+
+	public static void mainMenu() {
+		EventQueue.invokeLater(() -> {
+			mainMenu men = new mainMenu();
+			men.setVisible(true);
+			;
 		});
 	}
+
 	private void createLayout(JComponent... arg) {
 
-        Container pane = getContentPane();
-        GroupLayout gl = new GroupLayout(pane);
-        pane.setLayout(gl);
+		Container pane = getContentPane();
+		GroupLayout gl = new GroupLayout(pane);
+		pane.setLayout(gl);
 
-        gl.setAutoCreateContainerGaps(true);
+		gl.setAutoCreateContainerGaps(true);
 
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
+		gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(arg[0]));
 
-        gl.setVerticalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
-}
-	
+		gl.setVerticalGroup(gl.createSequentialGroup().addComponent(arg[0]));
+	}
+
 }
