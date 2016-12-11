63095ab0466e856d3c936776ba740831ed30168e
Aidan Sciortino <2001flyingpigs14@gmail.com>
2016-12-10 20:18:46 -0500

    Added class of border's i'm going to use
    Just making life easier

---
+++ src/openCivV/borders.java
@@ -0,0 +1,8 @@
+package openCivV;
+
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+public class borders {
+	public static Border empty = BorderFactory.createEmptyBorder();
+}
