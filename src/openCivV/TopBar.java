99110540c451a7cacf09d3cb321d2168d6a3e436
Aidan Sciortino <2001flyingpigs14@gmail.com>
2016-12-10 20:40:53 -0500

    Split TopBar into two parts
    Makes it easier in terms of alignment. If someone knows a better way to
    do this than putting together panels that'd be awesome

--- src/openCivV/TopBar.java
+++ src/openCivV/TopBar.java
@@ -1,5 +1,6 @@
 package openCivV;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -8,29 +9,46 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class TopBar extends JPanel {
 	public TopBar(){
-		FlowLayout rightLayout = new FlowLayout(FlowLayout.TRAILING);
-		this.setLayout(rightLayout);
-		this.setSize(1280,60);
+		this.setLayout(new BorderLayout());
+		this.setSize(vars.xdimen,60);
 		this.setBackground(Color.BLACK);
 		this.setBorder(borders.empty);
-		JButton menuButton = new customButton("Menu");
-		this.add(menuButton);
-		menuButton.addActionListener((ActionEvent event) -> {
-			mainMenu.mainMenu();
-		});
-		
+		this.add(new rightHalf(), BorderLayout.EAST);
+		this.add(new leftHalf(), BorderLayout.WEST);
+		this.setVisible(true);
 	}
 }
 class leftHalf extends JPanel {
 	public leftHalf(){
+		FlowLayout leftLayout = new FlowLayout(FlowLayout.LEADING);
+		this.setSize(vars.xdimen/2,60);
+		this.setBackground(Color.BLACK);
+		this.setBorder(borders.empty);
+		
+		JRadioButton scienceToggle = new JRadioButton();
+		scienceToggle.setToolTipText("Show or Hide Science Pane");
+		scienceToggle.setBackground(Color.black);
+		this.add(scienceToggle);
 		
 	}
 }
 class rightHalf extends JPanel {
 	public rightHalf(){
+		FlowLayout rightLayout = new FlowLayout(FlowLayout.TRAILING);
+		this.setSize(vars.xdimen/2,60);
+		this.setBackground(Color.BLACK);
+		this.setBorder(borders.empty);
 		
+		JButton menuButton = new customButton("Menu");
+		menuButton.setToolTipText("Show the Main Menu");
+		this.add(menuButton);
+		menuButton.addActionListener((ActionEvent event) -> {
+			mainMenu.mainMenu();
+		});
 	}
+	
 }
