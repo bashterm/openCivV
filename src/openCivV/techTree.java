7e295c95a43793004042736a849e3d92cee66598
890156128 <890156128@S067W100-S04.rcsd.dom>
2016-12-12 10:08:52 -0500

    Added tech tree button to science pane

--- src/openCivV/techTree.java
+++ src/openCivV/techTree.java
@@ -3,7 +3,7 @@ package openCivV;
 import javax.swing.JFrame;
 
 public class techTree {
-	public techTree(){
+	public techTree(String treeFile){
 		JFrame frame = new JFrame("Technology Tree");
 		frame.setSize(vars.xdimen/2,vars.ydimen/2);
 		frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
