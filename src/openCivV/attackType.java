38f856f189d9a574ddf198e4fe293eefcfdf8a90
890156128 <890156128@S067W100-S04.rcsd.dom>
2016-12-13 09:55:48 -0500

    Did things

---
+++ src/openCivV/attackType.java
@@ -0,0 +1,11 @@
+package openCivV;
+
+public class attackType {
+	
+	public attackType(boolean ranged, int strength){
+		
+	}
+	public attackType(int strength){
+		
+	}
+}
--- src/openCivV/unit.java
+++ src/openCivV/unit.java
@@ -1,5 +1,10 @@
 package openCivV;
 
 public class unit {
-
+	public unit(int moveSpeed, attackType attackType, int defenseStrength){
+		
+	}
+	public unit(int moveSpeed, attackType attackType){
+		
+	}
 }
